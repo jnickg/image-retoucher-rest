0439b3e4db1ccd04b2f7fcc2a0e4f276318907b6
jnickg <me@jnickg.net>
2022-07-30 21:17:45 -0800

    Add color transfer, collage endpoint, do some refactoring

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import io
-from typing import Callable, Tuple, Type
+from typing import Callable, Tuple, Type, List
 from time import perf_counter
 from flask import Flask, Response, url_for, request, jsonify, send_file
 import numpy as np
@@ -25,6 +25,26 @@ if __name__ != "__main__":
 #
 # Helpers & Constants
 #
+
+def normalize_data(data):
+    data_min = np.min(data)
+    data_max = np.max(data)
+    normalized = np.array((data - data_min) / (data_max - data_min), dtype=np.float32)
+    return normalized
+
+def do_color_transfer(src_img : np.ndarray, tgt_img : np.ndarray):
+    src_star = np.zeros_like(src_img, dtype=np.float32)
+    for i in range(3):
+        src_star[:,:,i] = src_img[:,:,i] - np.mean(src_img[:,:,i])
+    sigma_s = [np.std(src_img[:,:,i]) for i in range(3)]
+    sigma_t = [np.std(tgt_img[:,:,i]) for i in range(3)]
+    src_prime = np.zeros_like(src_img, dtype=np.float32)
+    for i in range(3):
+        src_prime[:,:,i] = sigma_t[i] / sigma_s[i] * src_star[:,:,i]
+    result = np.zeros_like(src_img, dtype=np.float32)
+    for i in range(3):
+        result[:,:,i] = src_prime[:,:,i] + np.mean(tgt_img[:,:,i])
+    return result
 
 def intparam2float(value:int) -> float:
     return float((value + 100) * 0.01 )
@@ -64,6 +84,16 @@ def apply_clahe(img: np.ndarray):
     lab_equalized = cv.merge((_l_equalized,_a,_b))
     img_equalized = cv.cvtColor(lab_equalized, cv.COLOR_LAB2BGR)
     return img_equalized
+
+def apply_colortransfer(src: np.ndarray, tgt_id:int):
+    tgt = get_image(tgt_id)
+
+    src_lab = cv.cvtColor(normalize_data(src), cv.COLOR_BGR2LAB)
+    tgt_lab = cv.cvtColor(normalize_data(tgt), cv.COLOR_BGR2LAB)
+    result_lab = do_color_transfer(src_lab, tgt_lab)
+    result = cv.cvtColor(result_lab, cv.COLOR_LAB2BGR)
+    result = (result * 255.0).astype(dtype=np.uint8)
+    return result
 
 def adjust_nop(img: np.ndarray, cmd, val):
     app.logger.debug(f'No-op executed for command \'{cmd}\' with parameter \'{val}\'')
@@ -110,6 +140,12 @@ COMMAND = {
         func = lambda i, _: apply_clahe(i),
         param_type = type(None),
         param_range = (None, None)
+    ),
+    'colorxfer': CommandDescriptor(
+        description="Apply color-transfer to the current image in CIELAB space, using the specified image ID",
+        func = lambda i, tgt_id: apply_colortransfer(i, int(tgt_id)),
+        param_type = int,
+        param_range = (0, None)
     )
 }
 
@@ -121,6 +157,10 @@ STATIC_IMAGES = {
     4 : 'static/moon.jpg',
     5 : 'static/snowman.jpg',
 }
+
+STATIC_COLLAGE = None
+
+STATIC_IMAGEDATA = [None] * len(STATIC_IMAGES.keys())
 
 def process_path(img: np.ndarray, path):
     i = np.array(path.split('/'))
@@ -140,15 +180,91 @@ def process_path(img: np.ndarray, path):
     return img
 
 def get_image(id:int) -> np.ndarray:
+    global STATIC_IMAGES
+    global STATIC_IMAGEDATA
+    
     static_path = STATIC_IMAGES.get(id)
     img = Response(ERROR_PAGE, 404)
     if (static_path is None):
         # TODO try retrieving key from postgres
         pass
     else:
+        if STATIC_IMAGEDATA[id] is not None:
+            return STATIC_IMAGEDATA[id]
         app.logger.info(f'Loading static resource {static_path}...')
         img = cv.imread(static_path, cv.IMREAD_COLOR)
+        STATIC_IMAGEDATA[id] = img
     return img
+
+def encode_image(img: np.ndarray) -> any:
+    _success, _buffer = cv.imencode('.png', img)
+    if not _success:
+        return Response(ERROR_PAGE, 500)
+
+    return send_file(
+        io.BytesIO(_buffer),
+        mimetype='image/png'
+    )
+
+def resize_image(img, size=(28,28)):
+    h, w = img.shape[:2]
+    c = img.shape[2] if len(img.shape)>2 else 1
+
+    if h == w: 
+        return cv.resize(img, size, cv.INTER_AREA)
+
+    dif = h if h > w else w
+
+    interpolation = cv.INTER_AREA if dif > (size[0]+size[1])//2 else cv.INTER_CUBIC
+
+    x_pos = (dif - w)//2
+    y_pos = (dif - h)//2
+
+    if len(img.shape) == 2:
+        mask = np.zeros((dif, dif), dtype=img.dtype)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w] = img[:h, :w]
+    else:
+        mask = np.zeros((dif, dif, c), dtype=img.dtype)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
+
+    return cv.resize(mask, size, interpolation)
+
+def make_collage(images:List[np.ndarray]) -> np.ndarray:
+    global STATIC_COLLAGE
+    
+    if STATIC_COLLAGE is not None:
+        return STATIC_COLLAGE
+    COLLAGE_SQUARE_SIDE = 400
+    COLLAGE_SQUARE_DIMS = (COLLAGE_SQUARE_SIDE, COLLAGE_SQUARE_SIDE)
+    COLLAGE_SQUARE_EMPTY = (COLLAGE_SQUARE_SIDE, COLLAGE_SQUARE_SIDE, 3)
+    images = [resize_image(_img, COLLAGE_SQUARE_DIMS) for _img in images]
+    if (len(images) % 2) != 0:
+        images.append(np.zeros(COLLAGE_SQUARE_EMPTY))
+    COLLAGE_RATIO = 3.0 / 5.0
+    img_count = len(images)
+    factors = []
+    for fac_1 in range(1, img_count+1):
+        if img_count % fac_1 == 0:
+            fac_2 = img_count // fac_1
+            factors.append((fac_1, fac_2))
+    factor_ratios = [(f1/f2) for (f1, f2) in factors]
+    ratio_dists = [abs(r - COLLAGE_RATIO) for r in factor_ratios]
+    best_dimensions = factors[np.argmin(ratio_dists)]
+    (h, w) = best_dimensions
+    h_px = h * COLLAGE_SQUARE_SIDE
+    w_px = w * COLLAGE_SQUARE_SIDE
+    app.logger.debug(f'Creating collage with dimensions h={h_px} w={w_px}')
+    collage = np.zeros((h_px, w_px, 3))
+    for h_i in range(h):
+        for w_i in range(w):
+            idx = (h_i * w) + w_i
+            y = h_i * COLLAGE_SQUARE_SIDE
+            x = w_i * COLLAGE_SQUARE_SIDE
+            collage[y:y+COLLAGE_SQUARE_SIDE,x:x+COLLAGE_SQUARE_SIDE,:] = images[idx]
+            collage = cv.putText(collage, f'{idx}', (x, y+50), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=2.0, color=(0,255,0), thickness=5)
+    STATIC_COLLAGE = collage
+    return collage
+
 
 ROOT_PAGE = """
 <h1 style='color:blue'>Image Retoucher REST API</h1>
@@ -162,7 +278,10 @@ ROOT_PAGE = """
 </ul>
 
 """
-ERROR_PAGE = "<h1 style='color:red'>Internal Server Error</h1>"
+
+ERROR_PAGE = """
+<h1 style='color:red'>Internal Server Error</h1>
+"""
 
 
 #
@@ -214,6 +333,13 @@ def api_image_static():
         "names": names
     })
 
+@app.route("/api/image/static/collage")
+def api_image_static_collage():
+    ids = STATIC_IMAGES.keys()
+    images = [get_image(_id) for _id in ids]
+    collage = make_collage(images)
+    return encode_image(collage)
+
 @app.route("/api/image/<int:id>/")
 def api_image_id_get(id:int):
     _img = get_image(id)
@@ -222,14 +348,7 @@ def api_image_id_get(id:int):
     if isinstance(_img, Response):
         return _img
 
-    _success, _buffer = cv.imencode('.png', _img)
-    if not _success:
-        return Response(ERROR_PAGE, 500)
-
-    return send_file(
-        io.BytesIO(_buffer),
-        mimetype='image/png'
-    )
+    return encode_image(_img)
 
 @app.route("/api/image/<int:id>/<path:path>")
 def api_image_id_get_path(id:int, path):
@@ -246,14 +365,7 @@ def api_image_id_get_path(id:int, path):
         app.logger.error(f'Failed to execute commands in URL. Error: {e}')
         return Response(ERROR_PAGE, 500)
 
-    _success, _buffer = cv.imencode('.png', _img)
-    if not _success:
-        return Response(ERROR_PAGE, 500)
-
-    return send_file(
-        io.BytesIO(_buffer),
-        mimetype='image/png'
-    )
+    return encode_image(_img)
 
 @app.route("/api/image/<int:id>/<path:path>/comparison")
 def api_image_id_get_path_comparison(id:int, path):
@@ -272,14 +384,7 @@ def api_image_id_get_path_comparison(id:int, path):
         return Response(ERROR_PAGE, 500)
 
     _stack = np.hstack((_orig, _img))
-    _success, _buffer = cv.imencode('.png', _stack)
-    if not _success:
-        return Response(ERROR_PAGE, 500)
-
-    return send_file(
-        io.BytesIO(_buffer),
-        mimetype='image/png'
-    )
+    return encode_image(_stack)
 
 @app.route("/api/image/<int:id>/<path:path>/save", methods=['POST'])
 def api_image_id_path_save(id:int, path):
